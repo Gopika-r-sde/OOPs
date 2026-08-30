e71d30136fbf4efdca41e57dd15fb532c48bd63c
gopika-r-sde <rgopikarajamani@gmail.com>
2026-08-30 19:52:37 +0530

    class and object creation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+class Student {
+    String name = "Ashok";
+    int age = 20;
+
+}
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Student student = new Student();
+        student.name = "Jack";
+        student.age = 18;
+        System.out.println(student.name);
+        System.out.println(student.age);
+
+
+        Student student2 = new Student();
+        System.out.println(student2.name);
+        System.out.println(student2.age);
     }
 }
