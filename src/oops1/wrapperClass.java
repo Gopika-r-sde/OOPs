39f213640d284d653576d7e8b77dd0bdf2d41919
gopika-r-sde <rgopikarajamani@gmail.com>
2026-08-30 20:40:02 +0530

    Wrapper Class

---
+++ src/oops1/wrapperClass.java
@@ -0,0 +1,35 @@
+package oops1;
+
+
+
+public class wrapperClass {
+    public static void main(String[] args) {
+
+        // Objects
+        Integer a = new Integer(1);  // Stores in heap memory as object
+        Integer b = 25;
+        swap(a, b);
+        System.out.println("Before swap " + a + " " + b);
+        System.out.println("After swap " + a + " " + b);
+
+
+        int x = 45;  // Stores in stack memory as a variable
+        int y = 50;
+        swap(x, y);
+        System.out.println("Before swap " + x + " " + y);
+        System.out.println("After swap " + x + " " + y);
+    }
+    // Primitives allow pass by value not pass by reference
+    static void swap(int x, int y) {
+        int temp = x;
+        x = y;
+        y = temp;
+    }
+
+    // Non-Primitives allow pass by reference
+    static void swap(Integer a, Integer b) {  // Integer class is "final" class, so no swap
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+}
