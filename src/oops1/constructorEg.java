338edbaeea32e8391ba2b39daac03aec80f1e7dd
gopika-r-sde <rgopikarajamani@gmail.com>
2026-08-30 20:18:58 +0530

    'new' memory allocation

--- src/oops1/constructorEg.java
+++ src/oops1/constructorEg.java
@@ -26,18 +26,24 @@ class  Human {
 }
 public class constructorEg {
     public static void main(String[] args) {
-        Human h = new Human();
-        System.out.println(h.name);
-        System.out.println(h.age);
-        System.out.println(h.gender);
-
-        Human h2 = new Human("Deepa", 18, "Female");
-        System.out.println(h2.name);
-        System.out.println(h2.age);
-        System.out.println(h2.gender);
-        h2.greeting();
+//        Human h = new Human();
+//        System.out.println(h.name);
+//        System.out.println(h.age);
+//        System.out.println(h.gender);
+//
+//        Human h2 = new Human("Deepa", 18, "Female");
+//        System.out.println(h2.name);
+//        System.out.println(h2.age);
+//        System.out.println(h2.gender);
+//        h2.greeting();
 
         Human h3 = new Human("Gopika", 20, "Male");
         h3.greeting();
+
+        // new -> allocates memory
+        Human h4 = h3;  // h4 points to the object that is pointing by h3
+        h4.greeting();
+
+        // the value changes in reference variable h3 will also affect the reference variable h4
     }
 }
