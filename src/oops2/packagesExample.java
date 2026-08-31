705e23ca4804b4628d58bb3834aff9633910f8f5
gopika-r-sde <rgopikarajamani@gmail.com>
2026-08-31 19:22:42 +0530

    this in static variable

--- src/oops2/packagesExample.java
+++ src/oops2/packagesExample.java
@@ -2,10 +2,36 @@ package oops2;
 
 import static oops1.Stud.greet;
 
+class Fun {
+    static String name = "Gops";
+
+    static void hi() {
+        System.out.println("Static method");
+
+        // name is not a static variable so that too shows error
+        System.out.println(Fun.name);  // error
+//        System.out.println(this.name);  // error
+
+        // this / super keywords represents an object, but static methods are not dependent on an object
+    }
+
+    void hello() {
+        System.out.println("Non-static method" + " " + this.name);
+    }
+}
+
 public class packagesExample {
     public static void main(String[] args) {
         greet();
         message();
+
+        // when a method is non-static
+        Fun fun = new Fun();
+        fun.hello();
+
+
+        // when a method is static
+        Fun.hi();
     }
 
     public static void message() {
