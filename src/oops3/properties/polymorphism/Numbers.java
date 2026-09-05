fd58a128b9764ec2db3e0f337438376db8fd524c
gopika-r-sde <rgopikarajamani@gmail.com>
2026-09-05 21:52:31 +0530

    Method Overloading

---
+++ src/oops3/properties/polymorphism/Numbers.java
@@ -0,0 +1,21 @@
+package oops3.properties.polymorphism;
+
+public class Numbers {
+    double sum(double a, int b) {
+        return a+b;
+    }
+    int sum(int a, int b){
+        return a+b;
+    }
+
+    int sum(int a, int b, int c){
+        return a+b+c;
+    }
+
+    static void main(String[] args) {
+        Numbers n = new Numbers();
+        System.out.println(n.sum(1,2,3));
+        System.out.println(n.sum(4,5));
+        System.out.println(n.sum(5.0,7));
+    }
+}
