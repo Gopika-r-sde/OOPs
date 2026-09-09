3bcbe18999588dc8b74a93dfae7bf0865b9d97b1
gopika-r-sde <rgopikarajamani@gmail.com>
2026-09-09 22:24:40 +0530

    Abstract Static Method

--- src/oops5/abstractDemo/Parent.java
+++ src/oops5/abstractDemo/Parent.java
@@ -10,6 +10,8 @@ public abstract class Parent {
 
 //    abstract Parent();  // cannot create abstract constructor
 
+//    abstract static void hi(); // cannot create abstract static
+
     abstract void career();
     abstract void partner();
 }
