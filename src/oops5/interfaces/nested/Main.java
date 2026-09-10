b7dde25ca8a6f1412e18a1c125481625b94d3562
gopika-r-sde <rgopikarajamani@gmail.com>
2026-09-10 21:55:05 +0530

    Nested Interface

---
+++ src/oops5/interfaces/nested/Main.java
@@ -0,0 +1,12 @@
+package oops5.interfaces.nested;
+
+public class Main {
+    public static void main(String[] args) {
+        B obj = new B();
+        System.out.println(obj.isOdd(6));
+        System.out.println(obj.isOdd(17));
+        System.out.println(obj.isOdd(12));
+        System.out.println(obj.isOdd(15));
+
+    }
+}
