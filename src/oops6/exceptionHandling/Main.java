cb2533aef3db57b3b0d69d868739ff63c84a94c9
gopika-r-sde <rgopikarajamani@gmail.com>
2026-09-13 23:42:29 +0530

    throw, throws

--- src/oops6/exceptionHandling/Main.java
+++ src/oops6/exceptionHandling/Main.java
@@ -5,12 +5,19 @@ public class Main {
         int a = 5;
         int b = 0;
         try {
-            int c = a / b;
+            divide(a, b);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         } finally {
             // finally block helps to close the opened file!!!
             System.out.println("Code Executed Successfully");
         }
+    }
+
+    static int divide(int a, int b) throws ArithmeticException{
+        if(b == 0) {
+            throw new ArithmeticException("Divide by zero");
+        }
+        return a / b;
     }
 }
