245cb0897e20f53fd87b6f1e6a7be37d99226b0c
gopika-r-sde <rgopikarajamani@gmail.com>
2026-09-13 23:11:09 +0530

    Lambda Function using custom interface

--- src/oops6/generics/LambdaFunctions.java
+++ src/oops6/generics/LambdaFunctions.java
@@ -13,8 +13,29 @@ public class LambdaFunctions {
 
 //        list.forEach((item) -> System.out.println(item + 1));
 
-        // Consumer -> Functional Interface
-        Consumer<Integer> consumer = (item) -> System.out.println(item * 2);
-        list.forEach(consumer);
+        // Consumer -> Functional Interface -> Accepts single input parameter and return no results
+//        Consumer<Integer> consumer = (item) -> System.out.println(item * 2);
+//        list.forEach(consumer);
+
+        Operation sum = (a, b) -> a + b;
+        Operation sub =  (a, b) -> a - b;
+        Operation prod = (a, b) -> a * b;
+        Operation div = (a, b) -> a / b;
+
+        LambdaFunctions obj = new LambdaFunctions();
+        System.out.println(obj.operate(5, 3, sum));
+        System.out.println(obj.operate(6, 2, sub));
+        System.out.println(obj.operate(2, 5, prod));
+        System.out.println(obj.operate(10000, 10, div));
+
     }
+
+    int operate(int a, int b, Operation op) {
+        return op.operation(a,b);
+    }
+
+}
+// It accepts two input arguments and also can return results
+interface Operation {
+    int operation(int a, int b);
 }
